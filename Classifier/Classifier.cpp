3b8e6a872ce825ea49200880516a2fe4a4e29569
FistCar <fistcar@hotmail.com>
2024-02-14 14:18:14 -0600

    Add project files.

---
+++ Classifier/Classifier.cpp
@@ -0,0 +1,12 @@
+﻿// Classifier.cpp : Defines the entry point for the application.
+//
+
+#include "Classifier.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
